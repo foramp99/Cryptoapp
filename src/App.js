93c6de6e6dc583d57ecbbd7606dd2d6cfe005c67
Foram Patel <82638008+foramp99@users.noreply.github.com>
2022-06-22 14:36:23 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+const App = () => {
+  return (
+    <div>
+      <h1>mkwdj</h1>
+    </div>
+  );
+};
+
+export default App;
